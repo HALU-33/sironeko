0beb3b49f4ef1653d389fd9da842be649141322f
HALU_33 <74422229+HALU-33@users.noreply.github.com>
2023-05-18 19:53:20 +0900

    Update BP_Calc.js

--- BP_Calc.js
+++ BP_Calc.js
@@ -1,7 +1,4 @@
-document.getElementById('calc-form').addEventListener('submit', function(event) {
-  event.preventDefault();
-  
-// 称号と累計BPの一覧
+d// 称号と累計BPの一覧
 const titles = [
   { name: "アストラ島の住人", totalBP: 0 },
   { name: "駆け出し冒険家9級", totalBP: 10 },
